9e58eccf499812c2e0ac96f6933da69a670c5243
Alphin Shajan <alphinshajan2004@gmail.com>
2024-12-28 19:03:01 -0500

    replaced 3D images

--- HomePage.js
+++ HomePage.js
@@ -1,27 +1,20 @@
-const pre = document.querySelector("pre");
+const githubLogo = document.querySelector(".github-logo");
+const linkedinLogo = document.querySelector(".linkedin-logo");
 
 document.addEventListener("mousemove", (e) => {
-  rotateElement(e, pre);
+    moveElement(e, githubLogo, 30);
+    moveElement(e, linkedinLogo, 45);
 });
 
-function rotateElement(event, element) {
-  // get mouse position
-  const x = event.clientX;
-  const y = event.clientY;
-  // console.log(x, y)
+function moveElement(event, element, intensity) {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  // find the middle
-  const middleX = window.innerWidth / 2;
-  const middleY = window.innerHeight / 2;
-  // console.log(middleX, middleY)
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
 
-  // get offset from middle as a percentage
-  // and tone it down a little
-  const offsetX = ((x - middleX) / middleX) * 45;
-  const offsetY = ((y - middleY) / middleY) * 45;
-  // console.log(offsetX, offsetY);
+    const offsetX = ((x - middleX) / middleX) * intensity;
+    const offsetY = ((y - middleY) / middleY) * intensity;
 
-  // set rotation
-  element.style.setProperty("--rotateX", offsetX + "deg");
-  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+    element.style.transform = `translate(${offsetX}px, ${-offsetY}px)`;
 }
